30e8c6525c8af01463281e0e9437ad1b0faf2cb6
DivineSavage <r.smiltins101@gmail.com>
2023-09-27 09:55:11 +0300

    Cl_

---
+++ Class_05/first.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+ {
+/*
+WADDUP
+ES VARU
+TAGAD
+RAKSTIT
+RINDAS VAIRAKAS
+*/
+
+
+// return 55; // NIHUJA ES TE VARU RAKSTIT LIETAS
+
+ printf("Hello World\n");
+
+
+ return 0;
+
+ }
