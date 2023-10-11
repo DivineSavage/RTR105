0162e3c2b522923c57f7cb0a0d6b01efa8d6ab20
DivineSavage <r.smiltins101@gmail.com>
2023-10-11 11:52:34 +0300

    uzd3

---
+++ Class_07/uzd3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main(void)
+ {
+ int a = 0;
+ int b = 0;
+ int c = 0;
+ char agdl;
+
+ printf("Ievadat vienu ciparu ");
+ scanf("%d",&a);
+ printf("Ievadat otru ciparu ");
+ scanf("%d",&b);
+ printf("Ievadat treso ciparu ");
+ scanf("%d",&c);
+ printf("To sakārtot augošā vai dilstošā secībā Ievadat > vai < ")
+ scanf("%c",&agdl)
+
+ if(agdl == >){
+  
+
+
+
+  }
+
+
+
+
+ return 0;
+ }
